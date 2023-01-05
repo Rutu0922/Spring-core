6d62fec7981f0b12510803935795292f59819fba
Rutu0922 <rutugondhali2605@gmail.com>
2023-01-05 14:01:14 +0530

    message

---
+++ src/main/java/com/jsp/demo/Pen.java
@@ -0,0 +1,13 @@
+package com.jsp.demo;
+
+public class Pen {
+ 
+	Pen() {
+		System.out.println("Constructor Loading");
+	}
+	 
+	public void write () {
+		System.out.println("Pen is writing");
+	}
+	
+}
